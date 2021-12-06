2ff2cfb2d7fb57db792894ee4bb49484000e3cec
Alok <sishu.chandra18@gmail.com>
2021-12-06 11:34:55 +0100

    setup project
    setup project

--- TestGitActions/ViewController.swift
+++ TestGitActions/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Project is ready :)")
 
         // Do any additional setup after loading the view.
     }
